fe5d71c689219e4a4587d70135e7b285ae8fc00d
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-11-10 22:03:00 +0800

    naokay na ang mobile view kol

--- admin/header.php
+++ admin/header.php
@@ -10,6 +10,8 @@ require ('../dbconnect.php');
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,1,0" />
