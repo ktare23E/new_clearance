df53e5e2b7b1ece3f993ecb45aac928eefd1f42a
ktare23 <tare.kristian@gmail.com>
2022-11-20 11:54:21 +0800

    new views

--- admin/student_view.php
+++ admin/student_view.php
@@ -78,7 +78,7 @@
                 <div class="student-registration">
                     <div class="student-view-profile-container">
                         <div class="student-view-profile-img-container">
-                            <img src="../images/dp.png" alt="">
+                            <img src="../images/yeah.jpg" alt="">
                         </div>
 
                         <div class="student-view-profile-info-container">
