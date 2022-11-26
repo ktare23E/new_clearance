ee94e827fd5e6d0f7c353285b5b6007897e9c488
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-11-27 00:53:36 +0800

    hello

--- admin/index.php
+++ admin/index.php
@@ -9,6 +9,12 @@
         ?>
         <!------------------ END OF ASIDE ---------------->
 
+        <div class="loader" id="loader">
+            <div class="center">
+                <div class="ring"></div>
+                <span id="loading-span">loading...</span>
+            </div>
+        </div>
         <main>
             <h1>Dashboard</h1>
 
@@ -225,7 +231,20 @@
         </div>
 
     </div>
-    
+    <script>
+        $(document).ready(function(){
+            const loadingSpan = document.getElementById("loading-span") 
+            const loader = document.getElementById("loader");
+            setTimeout(() => {
+                // loader.style.display = "none"
+                loadingSpan.innerHTML = "Welcome Kristianiter";
+                setTimeout(() => {
+                    loader.style.display = "none"
+                },1500)
+            }, 1500)
+        })
+        
+    </script>
     <script src="../assets/javascript/"></script>
     <script src="../assets/js/index.js"></script>
     
