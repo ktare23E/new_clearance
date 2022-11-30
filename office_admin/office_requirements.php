60b0e631c8f47fa5922f474dc8aad3d742128945
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-11-30 22:34:15 +0800

    bb

---
+++ office_admin/office_requirements.php
@@ -0,0 +1,23 @@
+<?php
+    include_once 'office_header.php';
+
+?>
+    <div class="office-container">
+        <?php 
+            include_once 'office_navtop.php'
+        ?>
+        
+        <!-- ================ MAIN =================== -->
+        <div class="main-requirements-container">
+            
+            
+        </div>
+
+        
+    </div>
+    
+    
+    <script src="./index.js"></script>
+    
+</body>
+</html>
