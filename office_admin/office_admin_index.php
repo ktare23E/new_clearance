b8ce019d3ba90ef78f5a94cba5d8d3704b28c717
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-12-02 23:48:50 +0800

    sa

--- office_admin/office_admin_index.php
+++ office_admin/office_admin_index.php
@@ -10,18 +10,68 @@
         <!-- ================ MAIN =================== -->
         <div class="office-main-container">
             <div class="first-div-container">
-                <div class="welcome-top">
-                    <div class="office-admin-image-container">
-                        <img src="../images/dp-2.jpg" alt="">
-                    </div>
+                <div class="top-left">
                     <div>Clearance at ease!</div>
-                    <h2>Nmsc Online Clearance System</h2>
+                    <h2><span class="primary">Nmsc</span> Online Clearance System</h2>
+                    <h3>Post clearance requirements and sign students' clearance online! All in one place!  </h3>
                 </div>
-                <div class="actions-container">
-                    <div class="action-container">
-
+                <div class="top-right">
+                    <div class="office-admin-image-container">
+                        <img src="../images/nmsc-logo.png" alt="">
                     </div>
                 </div>
+            </div>
+
+            <div class="actions-container">
+                <div class="action-container">
+                    <div class="circles-style">
+                        <div class="circle blue"></div>
+                        <div class="circle red"></div>
+                        <div class="circle orange"></div>
+                    </div>
+                    <div class="action-click">
+                        <span class="material-symbols-sharp">playlist_add</span>
+                        <h3>Add Requirements</h3>
+                    </div>
+                    <small>Updated version 2022</small>
+                </div>
+                <div class="action-container">
+                    <div class="circles-style">
+                        <div class="circle blue"></div>
+                        <div class="circle red"></div>
+                        <div class="circle orange"></div>
+                    </div>
+                    <div class="action-click">
+                        <span class="material-symbols-sharp">playlist_add</span>
+                        <h3>Add Requirements</h3>
+                    </div>
+                    <small>Updated version 2022</small>
+                </div>
+                <div class="action-container">
+                    <div class="circles-style">
+                        <div class="circle blue"></div>
+                        <div class="circle red"></div>
+                        <div class="circle orange"></div>
+                    </div>
+                    <div class="action-click">
+                        <span class="material-symbols-sharp">playlist_add</span>
+                        <h3>Add Requirements</h3>
+                    </div>
+                    <small>Updated version 2022</small>
+                </div>
+                <div class="action-container">
+                    <div class="circles-style">
+                        <div class="circle blue"></div>
+                        <div class="circle red"></div>
+                        <div class="circle orange"></div>
+                    </div>
+                    <div class="action-click">
+                        <span class="material-symbols-sharp">playlist_add</span>
+                        <h3>Add Requirements</h3>
+                    </div>
+                    <small>Updated version 2022</small>
+                </div>
+                    
             </div>
             
         </div>
