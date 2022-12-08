4d5921131ee3b23bb09f69d748b2ba01104aa047
ktare23 <tare.kristian@gmail.com>
2022-12-08 14:22:54 +0800

    bubu ko hahaha

--- admin/student_view.php
+++ admin/student_view.php
@@ -78,7 +78,7 @@
                 <div class="student-registration">
                     <div class="student-view-profile-container">
                         <div class="student-view-profile-img-container">
-                            <img src="<?= $row['student_profile']; ?>" alt="">
+                            <img src="uploads/<?= $row['student_profile']; ?>" alt="">
                         </div>
 
                         <div class="student-view-profile-info-container">
