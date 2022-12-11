fd5635e5b025b0a2b11443f6f88fbbc29051e4fa
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-12-12 07:58:25 +0800

    tabs

--- assets/js/student_index.js
+++ assets/js/student_index.js
@@ -42,21 +42,3 @@ if(activePage1 == "/new_clearance/student_user/student_user_index.php"){
 
 
 
-// tabs
-
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
-
-tabs.forEach(tab => {
-    tab.addEventListener('click', () => {
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
-        tabs.forEach(tab => {
-            tab.classList.remove('active')
-        })
-        tab.classList.add('active')
-        target.classList.add('active')
-    })
-})
