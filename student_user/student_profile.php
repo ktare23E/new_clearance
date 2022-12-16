81303d5a58c47a90eb8f5d3f4b641323b4c0d61e
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-12-16 20:06:38 +0800

    student profile

---
+++ student_user/student_profile.php
@@ -0,0 +1,76 @@
+<?php
+    include_once 'student_header.php';
+
+?>
+    <div class="office-container">
+        <?php 
+            include_once 'student_navtop.php'
+        ?>
+        
+        <!-- ================ MAIN =================== -->
+        <div class="office-main-container">
+            <div class="first-div-container">
+                <div style="width: 100%;"> 
+                    <h3 style="font-size: 2.5rem;text-align:center">Profile</h3>
+                </div>
+            </div>
+
+            <div class="student-profile-container">
+                <div class="profile-top-container">
+                    <div class="student-image" style="position: relative;">
+                        <img src="../images/profile-1.jpg" alt="">
+                        <div style="position: absolute;
+                            bottom: 5px;
+                            right: 5px;
+                        ">
+                            <button class="edit-profile-btn">
+                                <span class="material-symbols-sharp">border_color</span>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="name-student">
+                        <h1 style="font-size: 4rem;font-weight:500;margin-bottom:-15px;">AL CEDRIC DARIO</h1>
+                        <h3 style="font-size: 1.5rem;" class="text-muted">Student</h3>
+                    </div>
+                </div>
+                <div class="profile-main-info-container">
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Student id</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Year level</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Course</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Department</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Gender</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Email</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                    <div class="student-data-container">
+                        <h3 class="text-muted">Username</h3>
+                        <h4>AL CEDRIC DARIO</h4>
+                    </div>
+                </div>
+                
+            </div>
+
+        </div>
+    </div>
+    
+    
+    <script src="../assets/js/student_index.js"></script>  
+    
+</body>
+</html>
