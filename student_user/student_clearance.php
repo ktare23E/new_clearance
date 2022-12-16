5513862c30d4629f19b9af57d010a4d111ea7bb3
planetman219 <alcedric.dario@nmsc.edu.ph>
2022-12-16 19:13:18 +0800

    sa

--- student_user/student_clearance.php
+++ student_user/student_clearance.php
@@ -24,71 +24,70 @@
 
 
             <div class="clearance-section-container">
-                                    <div class="clearance-header-bar-container">
-                                        <h3>CLEARANCE INFORMATION</h3>
-                                        <div class="clearance-info-table-container">
-                                            <table>
-                                                <thead>
-                                                    <tr>
-                                                        <th>NAME</th>
-                                                        <th>COURSE</th>
-                                                        <th>YEAR</th>
-                                                        <th>STATUS</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th><?php if ($_SESSION['student_username']){
-                    echo $_SESSION['student_first_name'].' '.$_SESSION['student_last_name'];
-                } ?></th>
-                                                        <th><?php if ($_SESSION['student_username']){
-                    echo $_SESSION['student_first_name'];
-                } ?></th>
-                                                        <th>4TH</th>
-                                                        <th class="warning">PENDING</th>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                    <div class="clearance-details-container">
-                                        <h3>CLEARANCE DETAILS - SIGNING OFFICES STATUS</h3>
-                                        <div class="clearance-details-table-container">
-                                            <table>
-                                                <thead>
-                                                    <tr>
-                                                        <th>SIGNING OFFICE</th>
-                                                        <th>STATUS</th>
-                                                        <th>DATE SIGNED</th>
-                                                        
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th>CASHIER</th>
-                                                        <th class="warning">PENDING</th>
-                                                        <th>N/A</th>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>OSA</th>
-                                                        <th class="warning">PENDING</th>
-                                                        <th>N/A</th>
-                                                    </tr>
-                                                    <tr>
-                                                        <th>LIBRARY</th>
-                                                        <th class="success">CLEARED</th>
-                                                        <th>11/22/2022</th>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                
-            
-        </div>
+                <div class="clearance-header-bar-container">
+                    <h3>CLEARANCE INFORMATION</h3>
+                    <div class="clearance-info-table-container">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>NAME</th>
+                                    <th>COURSE</th>
+                                    <th>YEAR</th>
+                                    <th>STATUS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th><?php if ($_SESSION['student_username']){
+                                        echo $_SESSION['student_first_name'].' '.$_SESSION['student_last_name'];
+                                        } ?>
+                                    </th>
+                                    <th><?php if ($_SESSION['student_username']){
+                                        echo $_SESSION['student_first_name'];
+                                        } ?>
+                                    </th>
+                                    <th>4TH</th>
+                                    <th class="warning">PENDING</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
 
-        
+                <div class="clearance-details-container">
+                    <h3>CLEARANCE DETAILS - SIGNING OFFICES STATUS</h3>
+                    <div class="clearance-details-table-container">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>SIGNING OFFICE</th>
+                                    <th>STATUS</th>
+                                    <th>DATE SIGNED</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th>CASHIER</th>
+                                    <th class="warning">PENDING</th>
+                                    <th>N/A</th>
+                                </tr>
+                                <tr>
+                                    <th>OSA</th>
+                                    <th class="warning">PENDING</th>
+                                    <th>N/A</th>
+                                </tr>
+                                <tr>
+                                    <th>LIBRARY</th>
+                                    <th class="success">CLEARED</th>
+                                    <th>11/22/2022</th>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
     
     <script>
