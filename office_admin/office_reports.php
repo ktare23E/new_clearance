ac4387cebf3225893bc83fc5f9770010bb5e20a0
planetman219 <alcedric.dario@nmsc.edu.ph>
2023-04-11 12:36:18 +0800

    s

--- office_admin/office_reports.php
+++ office_admin/office_reports.php
@@ -14,31 +14,48 @@ $office_id = $_SESSION['office_id'];
 <html lang="en">
 
 <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,1,0" />
+        <!-- stylesheet -->
+        <link rel="stylesheet" href="../assets/css/office-admin-style.css">
+        <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+        <link rel="stylesheet" href="../assets/css//sweetalert2.min.css">
+        <script src="../assets/js//jquery.js"></script>
+        <script src="../assets/js//sweetalert2.all.min.js"></script>
+        <script defer src="../assets/js/active.js"></script>
+
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 </head>
+<body>
+        <img src="../images/report-back.jpg" alt="" class="image-back">
+        <div class="blur"></div>
 
-<body
->
-        <form action="retrieve_report.php" method="post">
-                <div class="input-field">
-                        <label for="">Select School Year And Sem</label>
-                        <select name="clearance_progress_id" id="" required>
-                                <option default>Select School Year And Sem</option>
-                                <?php $clearances = $db->result('clearance_progress_view'); ?>
-                                <?php foreach ($clearances as $clearance) : ?>
-                                        <?php $clearance->clearance_progress_id; ?>
-                                        <option value="<?= $clearance->clearance_progress_id; ?>"><?= $clearance->school_year_and_sem . ' ' . $clearance->sem_name; ?></option>
-                                <?php endforeach; ?>
-                        </select>
-                        <!-- <input type="hidden" name="office_id" value="<?= $office_id; ?>"> -->
-                </div>
-                <input type="submit" name="submit" value="submit">
-        </form>
+        <div class="select-report-container">
+                <form action="retrieve_report.php" method="post">
+                        <div class="report-form-container">
+                                <label for="">Select School Year And Sem</label>
+                                <select name="clearance_progress_id" id="" required>
+                                        <option default>Select School Year And Sem</option>
+                                        <?php $clearances = $db->result('clearance_progress_view'); ?>
+                                        <?php foreach ($clearances as $clearance) : ?>
+                                                <?php $clearance->clearance_progress_id; ?>
+                                                <option value="<?= $clearance->clearance_progress_id; ?>"><?= $clearance->school_year_and_sem . ' ' . $clearance->sem_name; ?></option>
+                                        <?php endforeach; ?>
+                                </select>
+                                <!-- <input type="hidden" name="office_id" value="<?= $office_id; ?>"> -->
+                        </div>
+                        <input type="submit" name="submit" value="submit">
+                </form>
+        </div>
+
+        <script>
+                document.title = "Reports"
+        </script>
 </body>
-
 </html>
