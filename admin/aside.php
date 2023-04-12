adec2e35f2c48962f92946cbecb0edaf25a7f5c0
planetman219 <alcedric.dario@nmsc.edu.ph>
2023-04-12 09:56:47 +0800

    d

--- admin/aside.php
+++ admin/aside.php
@@ -14,31 +14,6 @@
                     <span class="material-symbols-sharp">grid_view</span>
                     <h3>Dashboard</h3>
                 </a>
-                <a href="student.php">
-                    <span class="material-symbols-sharp">person_outline</span>
-                    <h3>Student</h3>
-                </a>
-                <!-- <a href="department.php">
-                    <span class="material-symbols-sharp">corporate_fare</span>
-                    <h3>Department</h3>
-                </a> -->
-                <a href="course.php">
-                    <span class="material-symbols-sharp">corporate_fare</span>
-                    <h3>Course</h3>
-                </a>
-                <a href="office.php">
-                    <span class="material-symbols-sharp">meeting_room</span>
-                    <h3>Office</h3>
-                </a>
-                <a href="office_account.php">
-                    <span class="material-symbols-sharp">meeting_room</span>
-                    <h3>Users Account</h3>
-                </a>
-                <a href="signing_office.php">
-                    <span class="material-symbols-sharp">edit_note</span>
-                    <h3>Signing Office</h3>
-                    
-                </a>
                 <a href="sy_sem.php">
                     <span class="material-symbols-sharp">calendar_month</span>
                     <h3>School Year</h3>
@@ -47,17 +22,43 @@
                     <span class="material-symbols-sharp">inventory</span>
                     <h3>Semester</h3>
                 </a>
-                <a href="clearance.php">
+                <a href="office.php">
+                    <span class="material-symbols-sharp">meeting_room</span>
+                    <h3>Office</h3>
+                </a>
+                <a href="course.php">
+                    <span class="material-symbols-sharp">corporate_fare</span>
+                    <h3>Course</h3>
+                </a>
+                <a href="office_account.php">
+                    <span class="material-symbols-sharp">meeting_room</span>
+                    <h3>Users Account</h3>
+                </a>
+                <a href="signing_office.php">
+                    <span class="material-symbols-sharp">edit_note</span>
+                    <h3>Signing Office</h3>
+                </a>
+                <a href="student.php">
+                    <span class="material-symbols-sharp">person_outline</span>
+                    <h3>Student</h3>
+                </a>
+                
+                <!-- <a href="department.php">
+                    <span class="material-symbols-sharp">corporate_fare</span>
+                    <h3>Department</h3>
+                </a> -->
+                
+                <a href="clearance_progress.php">
                     <span class="material-symbols-sharp">inventory</span>
-                    <h3>Clearance</h3>
+                    <h3>Clearance Period</h3>
                 </a>
                 <a href="clearance_type.php">
                     <span class="material-symbols-sharp">inventory</span>
                     <h3>Clearance Type</h3>
                 </a>
-                <a href="clearance_progress.php">
+                <a href="clearance.php">
                     <span class="material-symbols-sharp">inventory</span>
-                    <h3>Clearance Period</h3>
+                    <h3>Clearance</h3>
                 </a>
                 <a href="reports.php">
                     <span class="material-symbols-sharp">report</span>
