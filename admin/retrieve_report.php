43e385fa4a4f4b82553a18c6dc7476cea9629326
planetman219 <alcedric.dario@nmsc.edu.ph>
2023-04-13 16:07:33 +0800

    dssds

--- admin/retrieve_report.php
+++ admin/retrieve_report.php
@@ -11,6 +11,13 @@ if (isset($_POST['submit'])) {
     // $result = mysqli_query($conn, $query);
     // $row = mysqli_fetch_assoc($result);
 
+    $query2 = "SELECT * FROM clearance_progress_view WHERE clearance_progress_id = $clearance_progress_id";
+    $result2 = mysqli_query($conn, $query2);
+    $row2 = mysqli_fetch_assoc($result2);
+
+    $school_year_and_sem = $row2['school_year_and_sem'];  
+    $sem_name = $row2['sem_name'];
+
     $query = "SELECT * FROM office WHERE is_department = 1";
     $result = mysqli_query($conn, $query);
     $departments = array();
@@ -29,7 +36,7 @@ if (isset($_POST['submit'])) {
 <div class="reports-page-body">
     <div class="reports-main-container">
         <h1 style="text-align: center;">CLEARANCE REPORTS</h1>
-        <h1 style="text-align: center;margin-top:-20px">2020-2022 - 1st semester</h1>
+        <h1 style="text-align: center;margin-top:-20px"><?= $school_year_and_sem.' '.$sem_name; ?></h1>
         <button id="print-reports-button">Print Reports</button>
         <div class="canvas-container">
             <canvas id="myChart"></canvas>
