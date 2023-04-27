6676c19c9de1c0d9149951bdde747473374f0264
Kristian Kharl Tare <tare.kristian@gmail.com>
2023-04-27 16:04:55 +0800

    nice ka

--- admin/student.php
+++ admin/student.php
@@ -54,7 +54,7 @@
                                     <button id="add-new-student"><span class="material-symbols-sharp">add</span>Student</button>
                                 </a>
                             </div>
-                            <button class="download-csv" data-modal-target="#csv-download-modal">Download CSV</button>
+                            <button class="download-csv" data-modal-target="#create-clearance">Download CSV</button>
 
                             <button id="register-csv-file-btn"><span class="material-symbols-sharp">upload_file</span>Register Via .csv file<span class="material-symbols-sharp">arrow_forward_ios</span></button>
                             <div>
